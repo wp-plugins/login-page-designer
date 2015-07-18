e92f80815fa1c734805c386389c2e3eba88bc06c
chandrakeshkumar <chandrakeshkumar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-18 06:17:47 +0000

    Login page designer provides you to easy way to customize the appearance of the wordPress login page with many options.
    git-svn-id: https://plugins.svn.wordpress.org/login-page-designer/trunk@1201460 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/social-media.php
@@ -0,0 +1,20 @@
+<style>
+.wlpd_social_media{
+	  margin: 14px 0px;
+  margin-bottom: 9px;
+}
+</style>
+
+<div class="wlpd_social_media">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=166144553594684";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like" data-href="https://www.facebook.com/pages/WPChandra-To-Start-for-Web-Development/325741047605388" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+
+</div>
